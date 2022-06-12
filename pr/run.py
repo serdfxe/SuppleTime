451286eb4b904f02277095ce06de8db72172dfe7
pavel <pavel@DESKTOP-33FO31S>
2022-06-12 12:20:46 +0300

    main

--- pr/run.py
+++ pr/run.py
@@ -3,9 +3,5 @@ from SuppleTime.pr.app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host='0.0.0.0') #123123
-=======
-    app.run(host='0.0.0.0')
 #tolya loh hehehe
->>>>>>> ae4d5201844e5f7ad1db57c54ecdc042054a859c
