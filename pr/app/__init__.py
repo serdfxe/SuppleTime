9e574691343706a6ffb9820e048f652b12c9757f
serdfx <pavel_voronin@outlook.com>
2022-06-25 13:47:09 +0300

    main

--- pr/app/__init__.py
+++ pr/app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from SuppleTime.pr.app.views.main import main
 from SuppleTime.pr.app.views.admin import admin
 from SuppleTime.pr.app.views.auth import auth
+from SuppleTime.pr.app.views.root import root
 
 def create_app():
     app = Flask(__name__)
     app.debug = 0
     app.secret_key = b'sheeeeeeeeeesh'
 
-    app.register_blueprint(main)
+    app.register_blueprint(root)
+    app.register_blueprint(main, url_prefix='/app')
     app.register_blueprint(admin, url_prefix='/admin')
     app.register_blueprint(auth, url_prefix='/auth')
 
--- pr/app/views/auth.py
+++ pr/app/views/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 
 auth = Blueprint("auth", __name__)
@@ -9,7 +9,8 @@ def signup_route():
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
-        pass
+        flash("EROR", "eror")
+        return render_template("signup.html")
         
         
 @auth.route('/signin', methods=("GET", "POST"))
@@ -17,10 +18,10 @@ def signin_route():
     if request.method == "GET":
         return render_template("signin.html")
     if request.method == "POST":
-        pass
+        flash("EROR", "eror")
+        return render_template("signin.html")
 
 
 @auth.route('/', methods=("GET", "POST"))
 def auth_rout():
     return redirect(url_for("auth.signin_route"))
-
